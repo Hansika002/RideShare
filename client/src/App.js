f7468e8ea28f95fb662051c42948c25ea9444df7
hansika <hansika.amrutkar@gmail.com>
2024-03-10 20:35:24 +0530

    App.js changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Main from "./components/Main";
 import Signup from "./components/Singup";
 import Login from "./components/Login";
+import Search from "./components/Pages/Search/search";
+import Add from "./components/Pages/Add/add";
 
 function App() {
 	const user = localStorage.getItem("token");
@@ -12,6 +14,8 @@ function App() {
 			<Route path="/signup" exact element={<Signup />} />
 			<Route path="/login" exact element={<Login />} />
 			<Route path="/" element={<Navigate replace to="/login" />} />
+			<Route path="/search" element={<Search />} />
+			<Route path="/add" element={<Add />} />
 		</Routes>
 	);
 }
